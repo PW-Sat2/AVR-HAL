a3fe7d13508e9effb2fd9a93accccb360a85dca4
unknown <michal.gumiela@gmail.com>
2015-08-06 14:36:58 +0200

    Uncommented macro bit_is_clear, minor formatting issues.

--- HAL/AVR/bit_operations.h
+++ HAL/AVR/bit_operations.h
@@ -1,11 +1,10 @@
-#ifndef HAL_AVR_BIT_OPERATIONS_H_
-#define  HAL_AVR_BIT_OPERATIONS_H_
+#ifndef AVR_BIT_OPERATIONS_H_
+#define AVR_BIT_OPERATIONS_H_
 
 
 #define sbi(reg, bit) ((reg) |= (1 << (bit)))
 #define cbi(reg, bit) ((reg) &= (~(1 << (bit))))
 #define read_bit(reg, bit)  (((reg) >> bit) & 0x1)
+#define bit_is_clear(sfr, bit) (!(sfr & (1 << bit)))
 
-// #define bit_is_clear(sfr,bit) (!(sfr & (1 << bit)))
-
-#endif  // HAL_AVR_BIT_OPERATIONS_H_
+#endif  // AVR_BIT_OPERATIONS_H_
