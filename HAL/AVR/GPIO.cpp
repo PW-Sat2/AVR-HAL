468ad2230322effbf3e91877b46c42b761794c23
unknown <michal.gumiela@gmail.com>
2015-08-06 16:28:42 +0200

    this pointer added in some places.

--- HAL/AVR/GPIO.cpp
+++ HAL/AVR/GPIO.cpp
@@ -19,23 +19,23 @@ void GPIOPin_t::init(GPIOPin_t::InitTypeDef_t InitTypeDef) {
     this->pin_reg.PORTx = reinterpret_cast<volatile uint8_t*>pgm_read_byte(&(GPIOPin_t_descr[InitTypeDef.pin_nr].PORTx));
 
     if (InitTypeDef.mode == input) {
-        cbi((*pin_reg.DDRx), pin_reg.pin);
-        cbi((*pin_reg.PORTx), pin_reg.pin);
+        cbi((*this->pin_reg.DDRx), this->pin_reg.pin);
+        cbi((*this->pin_reg.PORTx), this->pin_reg.pin);
     } else if (InitTypeDef.mode == input_pullup) {
-        cbi((*pin_reg.DDRx), pin_reg.pin);
-        sbi((*pin_reg.PORTx), pin_reg.pin);
+        cbi((*this->pin_reg.DDRx), this->pin_reg.pin);
+        sbi((*this->pin_reg.PORTx), this->pin_reg.pin);
     } else {
-        sbi((*pin_reg.DDRx), pin_reg.pin);
-        cbi((*pin_reg.PORTx), pin_reg.pin);
+        sbi((*this->pin_reg.DDRx), this->pin_reg.pin);
+        cbi((*this->pin_reg.PORTx), this->pin_reg.pin);
     }
 }
 
 void GPIOPin_t::set() {
-    sbi((*pin_reg.PORTx), pin_reg.pin);
+    sbi((*this->pin_reg.PORTx), this->pin_reg.pin);
 }
 
 void GPIOPin_t::reset() {
-    cbi((*pin_reg.PORTx), pin_reg.pin);
+    cbi((*this->pin_reg.PORTx), this->pin_reg.pin);
 }
 
 void GPIOPin_t::write(bool value) {
@@ -47,7 +47,7 @@ void GPIOPin_t::write(bool value) {
 }
 
 bool GPIOPin_t::read() {
-    if (bit_is_clear((*pin_reg.PINx), pin_reg.pin)) {
+    if (bit_is_clear((*this->pin_reg.PINx), this->pin_reg.pin)) {
         return true;
     }
     return false;
