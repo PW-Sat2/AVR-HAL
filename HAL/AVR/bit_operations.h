ae780deb862c3ac3da7b2aa10a4b3d46a1b7a9d8
unknown <michal.gumiela@gmail.com>
2015-08-06 14:50:11 +0200

    bit_is_clear is already defined.

--- HAL/AVR/bit_operations.h
+++ HAL/AVR/bit_operations.h
@@ -5,6 +5,5 @@
 #define sbi(reg, bit) ((reg) |= (1 << (bit)))
 #define cbi(reg, bit) ((reg) &= (~(1 << (bit))))
 #define read_bit(reg, bit)  (((reg) >> bit) & 0x1)
-#define bit_is_clear(sfr, bit) (!(sfr & (1 << bit)))
 
 #endif  // AVR_BIT_OPERATIONS_H_
