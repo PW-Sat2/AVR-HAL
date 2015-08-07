84dbd2a78e17d02710a57c34c66425e8f1ed51cf
Grzegorz Gajoch <gajoch@gmail.com>
2015-08-07 21:23:19 +0200

    SPI functionality added. Testing necessary.

--- HAL/AVR/SPI.cpp
+++ HAL/AVR/SPI.cpp
@@ -1,28 +1,26 @@
 #include <avr/io.h>
 #include "SPI.h"
-#include "bit_operations.h"
+#include "pins.h"
 
 SPI_t::SPI_t() {
-
-}
-
-SPI_t::SPI_t(SPI_t::InitTypeDef_t InitStruct) {
-    this->init(InitStruct);
 }
 
 
 void SPI_t::init(SPI_t::InitTypeDef_t InitStruct) {
-    pin_mosi.init({InitStruct.pin_mosi, GPIOPin_t::output});
-    pin_miso.init({InitStruct.pin_miso, GPIOPin_t::input});
-    pin_sck.init({InitStruct.pin_sck, GPIOPin_t::output});
+    pin_mosi.init({SPIPin_Descr.pin_mosi, GPIOPin_t::output});
+    pin_miso.init({SPIPin_Descr.pin_miso, GPIOPin_t::input});
+    pin_sck.init({SPIPin_Descr.pin_sck, GPIOPin_t::output});
 
-    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1) | (1 << SPR0) | (1 << CPHA);  // master, fclk = fosc/128
+    SPCR = (1 << SPE) | (1 << MSTR) | (InitStruct.clk_divisor) |
+           (InitStruct.phase << CPHA) | (InitStruct.polarity << CPOL) |
+           (InitStruct.data_order << DORD);
 }
 
 
 uint8_t SPI_t::shift(uint8_t data) {
     SPDR = data;
-    while(!(SPSR & (1<<SPIF)));
+    while (!(SPSR & (1 << SPIF))) {
+    }
     return SPDR;
 }
 
@@ -33,31 +31,23 @@ SPI_Device_t::SPI_Device_t() {
 }
 
 SPI_Device_t::SPI_Device_t(uint8_t cs_pin) {
-	
-	this->init(cs_pin);
-
+    this->init(cs_pin);
 }
 
 void SPI_Device_t::init(uint8_t cs_pin) {
-	
-	this->pin_cs.init({cs_pin, GPIOPin_t::output});
-
+    this->pin_cs.init({cs_pin, GPIOPin_t::output});
 }
 
 void SPI_Device_t::enable() {
-	
-	this->pin_cs.reset();
-	
+    this->pin_cs.reset();
 }
 
 void SPI_Device_t::disable() {
-	
-	this->pin_cs.set();
-	
+    this->pin_cs.set();
 }
 
 uint8_t SPI_Device_t::shift(uint8_t data) {
-	return SPI.shift(data);
+    return SPI.shift(data);
 }
 
 uint8_t SPI_Device_t::data_transfer(uint8_t data) {
@@ -66,3 +56,4 @@ uint8_t SPI_Device_t::data_transfer(uint8_t data) {
     this->disable();
     return x;
 }
+
--- HAL/AVR/SPI.h
+++ HAL/AVR/SPI.h
@@ -4,40 +4,64 @@
 #include <stdint.h>
 #include "GPIO.h"
 
+struct SPIPinDescr_t {
+    uint8_t pin_mosi, pin_miso, pin_sck;
+};
 
 class SPI_t {
-private:
+ private:
     GPIOPin_t pin_mosi, pin_miso, pin_sck;
-public:
+
+ public:
+    enum SPI_Polarity {
+        SPI_idle_low = 0,
+        SPI_idle_high = 1
+    };
+
+    enum SPI_Phase {
+        SPI_leading_sample = 0,
+        SPI_trailing_sample = 1
+    };
+
+    enum SPI_DataOrder {
+        SPI_MSB_First = 0,
+        SPI_LSB_First = 1
+    };
+
+    enum SPI_Clock_Divisor {
+        SPI_Clk_Div_4 = 0,
+        SPI_Clk_Div_16 = 1,
+        SPI_Clk_Div_64 = 2,
+        SPI_Clk_Div_128 = 3,
+    };
+
     struct InitTypeDef_t {
-        uint8_t pin_mosi, pin_miso, pin_sck;
+        SPI_Polarity polarity;
+        SPI_Phase phase;
+        SPI_DataOrder data_order;
+        SPI_Clock_Divisor clk_divisor;
     };
 
     SPI_t();
-    explicit SPI_t(SPI_t::InitTypeDef_t InitStruct);
     void init(SPI_t::InitTypeDef_t InitStruct);
-	uint8_t shift(uint8_t data);
+    uint8_t shift(uint8_t data);
 };
 
 extern SPI_t SPI;
 
 
 class SPI_Device_t {
-
-private:
-
+ private:
     GPIOPin_t pin_cs;
-	
-public:
-	
-	SPI_Device_t();
-	
-	explicit SPI_Device_t(uint8_t cs_pin);
+
+ public:
+    SPI_Device_t();
+    explicit SPI_Device_t(uint8_t cs_pin);
     void enable(void);
     void disable(void);
     uint8_t shift(uint8_t data);
     uint8_t data_transfer(uint8_t data);
-	void init(uint8_t cs_pin);
+    void init(uint8_t cs_pin);
 };
 
 #endif  // HAL_AVR_SPI_H_
