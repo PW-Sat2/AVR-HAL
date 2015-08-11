a7732ebbea11021702fd539521ce70832051508a
unknown <michal.gumiela@gmail.com>
2015-08-11 17:05:31 +0200

    The code has been cleansed to meet linter expectations.

--- HAL/AVR/Serial.h
+++ HAL/AVR/Serial.h
@@ -1,182 +1,157 @@
 /*
  * Serial.h
  *
- * Created: 2012-12-29 02:27:29
- *  Author: rexina
+ *  Created: 2012-12-29 02:27:29
+ *  Author: Grzegorz Gajoch
  */ 
 
-#ifndef SERIAL_H_
-#define SERIAL_H_
+#ifndef HAL_AVR_SERIAL_H_
+#define HAL_AVR_SERIAL_H_
 
 #include <stdlib.h>
 #include "GPIO.h"
-
-
 #include "pins.h"
 #include "serial_mcu.h"
 
 
 template <int SERIALNUM>
-struct SerialX_
-{
+struct SerialX_ {
     char buffer[34];
-	
-    void begin(uint32_t _b)
-    {   
+
+    void begin(uint32_t _b) {
         /* Set baud rate */
         uint16_t ubrr;
         ubrr = static_cast<uint16_t>((
                static_cast<float>(
                static_cast<float>(F_CPU / 16.)/ (_b))) - 0.5);
-               
+
         UBRR0H = static_cast<uint8_t>(ubrr >> 8);
         UBRR0L = static_cast<uint8_t>(ubrr);
         REGGEN(UBRR0H) = (uint8_t) (ubrr >> 8);
         REGGEN(UBRR0L) = (uint8_t) ubrr;
-				
-        /* Enable receiver and transmitter */ 
+
+        /* Enable receiver and transmitter */
         REGGEN(UCSR0B) = (1 << REGGEN(RXEN0)) | (1 << REGGEN(TXEN0));
         /* Set frame format: 8data, 1stop bit */
         REGGEN(UCSR0C) = (1 << REGGEN(UCSZ01)) | (1 << REGGEN(UCSZ00));
     }
-    void newline()
-    {
+    void newline() {
         this->put((uint8_t) '\r');
         this->put((uint8_t) '\n');
     }
-    void put(uint8_t data)
-    {
-        while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0))));
+
+    void put(uint8_t data) {
+        while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0)))) {
+        }
         REGGEN(UDR0) = data;
     }
-    void put(char data)
-    {
-        while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0))));
+    void put(char data) {
+        while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0)))) {
+        }
         REGGEN(UDR0) = data;
     }
-    void print(char data)
-    {
+    void print(char data) {
         this->put(data);
     }
-    void println(char data)
-    {
+    void println(char data) {
         this->put(data);
         this->newline();
     }
-    void print(uint8_t data)
-    {
+    void print(uint8_t data) {
         this->put(data);
     }
-    void println(uint8_t data)
-    {
+    void println(uint8_t data) {
         this->put(data);
         this->newline();
     }
-    void print(char * out)
-    {
-        while (*out)
-        {
+    void print(char * out) {
+        while (*out) {
             /* Wait for empty transmit buffer */
-            while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0))));
+            while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0)))) {
+            }
             /* Put data into buffer, sends the data */
             REGGEN(UDR0) = *out;
             out++;
         }
     }
-    void print(char * out, uint16_t len)
-    {
+    void print(char * out, uint16_t len) {
         uint8_t i = 0;
-        while (i != len)
-        {
+        while (i != len) {
             /* Wait for empty transmit buffer */
-            while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0))));
+            while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0)))) {
+            }
             /* Put data into buffer, sends the data */
             REGGEN(UDR0) = *out;
             ++out;
             ++i;
         }
     }
-    void println(char * out)
-    {
+    void println(char * out) {
         this->print(out);
         this->newline();
     }
-    void print(uint16_t data)
-    {
+    void print(uint16_t data) {
         itoa(data, this->buffer, 10);
         this->print(this->buffer);
     }
-    void print(int data)
-    {
+    void print(int data) {
         itoa(data, this->buffer, 10);
         this->print(this->buffer);
     }
-    void println(int16_t data)
-    {
+    void println(int16_t data) {
         itoa(data, this->buffer, 10);
         this->print(this->buffer);
         this->newline();
     }
-    void println(uint16_t data)
-    {
+    void println(uint16_t data) {
         itoa(data, this->buffer, 10);
         this->print(this->buffer);
         this->newline();
     }
-    void print(uint32_t data)
-    {
+    void print(uint32_t data) {
         itoa(data, this->buffer, 10);
         this->print(this->buffer);
     }
-    void print(int32_t data)
-    {
-        //itoa(data, this->buffer, 10);
+    void print(int32_t data) {
         sprintf(this->buffer, "%ld", data);
         this->print(this->buffer);
     }
-    void println(int32_t data)
-    {
-        //itoa(data, this->buffer, 10);
+    void println(int32_t data) {
         sprintf(this->buffer, "%ld", data);
         this->print(this->buffer);
         this->newline();
     }
-    void println(uint32_t data)
-    {
+    void println(uint32_t data) {
         itoa(data, this->buffer, 10);
         this->print(this->buffer);
         this->newline();
     }
-    void print(float data)
-    {
+    void print(float data) {
         itoa(data, this->buffer, 10);
         this->print(this->buffer);
     }
-    void println(float data)
-    {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-        this->newline();
-    }
-    void print(double data)
-    {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-    }
-    void println(double data)
-    {
+    void println(float data) {
         itoa(data, this->buffer, 10);
         this->print(this->buffer);
         this->newline();
     }
-    bool available()
-    {
-        return ((REGGEN(UCSR0A) & (1 << REGGEN(RXC0))));
+    void print(double data) {
+        itoa(data, this->buffer, 10);
+        this->print(this->buffer);
     }
-    uint8_t read()
-    {
+    void println(double data) {
+        itoa(data, this->buffer, 10);
+        this->print(this->buffer);
+        this->newline();
+    }
+    bool available() {
+        return ((REGGEN(UCSR0A) & (1 << REGGEN(RXC0)))) {
+        }
+    }
+    uint8_t read() {
         /* Wait for data to be received */
-        while (!(REGGEN(UCSR0A) & (1 << REGGEN(RXC0))));
+        while (!(REGGEN(UCSR0A) & (1 << REGGEN(RXC0)))) {
+        }
         /* Get and return received data from buffer */
         return REGGEN(UDR0);
     }
@@ -199,4 +174,4 @@ SerialX_<2> Serial1;
 SerialX_<3> Serial1;
 #endif
 
-#endif /* SERIAL_H_ */
+#endif  //  HAL_AVR_SERIAL_H_
