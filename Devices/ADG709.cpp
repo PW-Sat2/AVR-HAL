1b9c5fc9d30bca23ca6ae783dd2b9e7ff0ae3184
Grzegorz Gajoch <gajoch@gmail.com>
2015-08-13 22:51:02 +0200

    changed Init to const reference

--- Devices/ADG709.cpp
+++ Devices/ADG709.cpp
@@ -3,11 +3,11 @@
 ADG709_t::ADG709_t() {
 }
 
-ADG709_t::ADG709_t(ADG709_t::InitTypeDef_t InitStruct) {
+ADG709_t::ADG709_t(const ADG709_t::InitTypeDef_t & InitStruct) {
     this->init(InitStruct);
 }
 
-void ADG709_t::init(InitTypeDef_t InitStruct) {
+void ADG709_t::init(const InitTypeDef_t & InitStruct const ) {
     this->A0.init({InitStruct.pin_A0, GPIOPin_t::output});
     this->A1.init({InitStruct.pin_A1, GPIOPin_t::output});
     this->EN.init({InitStruct.pin_EN, GPIOPin_t::output});
@@ -22,7 +22,7 @@ void ADG709_t::disable() {
     this->EN.reset();
 }
 
-void ADG709_t::select(uint8_t channel) {
+void ADG709_t::select(const uint8_t channel) {
     this->A0.write(static_cast<bool>(channel & 0b01));
     this->A1.write(static_cast<bool>(channel & 0b10));
 }
