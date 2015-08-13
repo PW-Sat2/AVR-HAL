2a03799e299e5a4ee9e20b44868b87d4b42ef785
Grzegorz Gajoch <gajoch@gmail.com>
2015-08-13 22:22:44 +0200

    deleted specific objects functions

--- HAL/AVR/Serial.h
+++ HAL/AVR/Serial.h
@@ -35,35 +35,7 @@ struct SerialX_ {
         /* Set frame format: 8data, 1stop bit */
         REGGEN(UCSR0C) = (1 << REGGEN(UCSZ01)) | (1 << REGGEN(UCSZ00));
     }
-    void newline() {
-        this->put((uint8_t) '\r');
-        this->put((uint8_t) '\n');
-    }
 
-    void put(uint8_t data) {
-        while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0)))) {
-        }
-        REGGEN(UDR0) = data;
-    }
-    void put(char data) {
-        while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0)))) {
-        }
-        REGGEN(UDR0) = data;
-    }
-    void print(char data) {
-        this->put(data);
-    }
-    void println(char data) {
-        this->put(data);
-        this->newline();
-    }
-    void print(uint8_t data) {
-        this->put(data);
-    }
-    void println(uint8_t data) {
-        this->put(data);
-        this->newline();
-    }
     void print(char * out) {
         while (*out) {
             /* Wait for empty transmit buffer */
@@ -74,75 +46,6 @@ struct SerialX_ {
             out++;
         }
     }
-    void print(char * out, uint16_t len) {
-        uint8_t i = 0;
-        while (i != len) {
-            /* Wait for empty transmit buffer */
-            while (!(REGGEN(UCSR0A) & (1 << REGGEN(UDRE0)))) {
-            }
-            /* Put data into buffer, sends the data */
-            REGGEN(UDR0) = *out;
-            ++out;
-            ++i;
-        }
-    }
-    void println(char * out) {
-        this->print(out);
-        this->newline();
-    }
-    void print(uint16_t data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-    }
-    void print(int data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-    }
-    void println(int16_t data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-        this->newline();
-    }
-    void println(uint16_t data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-        this->newline();
-    }
-    void print(uint32_t data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-    }
-    void print(int32_t data) {
-        sprintf(this->buffer, "%ld", data);
-        this->print(this->buffer);
-    }
-    void println(int32_t data) {
-        sprintf(this->buffer, "%ld", data);
-        this->print(this->buffer);
-        this->newline();
-    }
-    void println(uint32_t data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-        this->newline();
-    }
-    void print(float data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-    }
-    void println(float data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-        this->newline();
-    }
-    void print(double data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-    }
-    void println(double data) {
-        itoa(data, this->buffer, 10);
-        this->print(this->buffer);
-        this->newline();
     }
     bool available() {
         return ((REGGEN(UCSR0A) & (1 << REGGEN(RXC0))));
