4e3c8aaa9278fedaa225d50871ce3f94c8826400
Michał Gumiela <michal.gumiela@gmail.com>
2016-04-20 13:26:37 +0200

    Initial version of Arduino pins.

---
+++ HAL/AVR/MCU/ATMega2560/arduino.h
@@ -0,0 +1,62 @@
+#ifndef ARDUINO_H_
+#define ARDUINO_H_
+
+struct Arduino_t {
+    // Digital pins
+    static constexpr uint8_t D0 = 2;
+    static constexpr uint8_t D1 = 3;
+    static constexpr uint8_t D2 = 6;
+    static constexpr uint8_t D3 = 7;
+    static constexpr uint8_t D4 = 1;
+    static constexpr uint8_t D5 = 5;
+    static constexpr uint8_t D6 = 15;
+    static constexpr uint8_t D7 = 16;
+    static constexpr uint8_t D8 = 17;
+    static constexpr uint8_t D9 = 18;
+    static constexpr uint8_t D10 = 23;
+    static constexpr uint8_t D11 = 24;
+    static constexpr uint8_t D12 = 25;
+    static constexpr uint8_t D13 = 26;
+    static constexpr uint8_t D14 = 64;
+    static constexpr uint8_t D15 = 63;
+    static constexpr uint8_t D16 = 13;
+    static constexpr uint8_t D17 = 12;
+    static constexpr uint8_t D18 = 46;
+    static constexpr uint8_t D19 = 45;
+    static constexpr uint8_t D20 = 44;
+    static constexpr uint8_t D21 = 43;
+    static constexpr uint8_t D22 = 78;
+    static constexpr uint8_t D23 = 77;
+    static constexpr uint8_t D24 = 76;
+    static constexpr uint8_t D25 = 75;
+    static constexpr uint8_t D26 = 74;
+    static constexpr uint8_t D27 = 73;
+    static constexpr uint8_t D28 = 72;
+    static constexpr uint8_t D29 = 71;
+    static constexpr uint8_t D30 = 60;
+    static constexpr uint8_t D31 = 59;
+    static constexpr uint8_t D32 = 58;
+    static constexpr uint8_t D33 = 57;
+    static constexpr uint8_t D34 = 56;
+    static constexpr uint8_t D35 = 55;
+    static constexpr uint8_t D36 = 54;
+    static constexpr uint8_t D37 = 53;
+    static constexpr uint8_t D38 = 50;
+    static constexpr uint8_t D39 = 70;
+    static constexpr uint8_t D40 = 52;
+    static constexpr uint8_t D41 = 51;
+    static constexpr uint8_t D42 = 42;
+    static constexpr uint8_t D43 = 41;
+    static constexpr uint8_t D44 = 40;
+    static constexpr uint8_t D45 = 39;
+    static constexpr uint8_t D46 = 38;
+    static constexpr uint8_t D47 = 37;
+    static constexpr uint8_t D48 = 36;
+    static constexpr uint8_t D49 = 35;
+    static constexpr uint8_t D50 = 22;
+    static constexpr uint8_t D51 = 21;
+    static constexpr uint8_t D52 = 20;
+    static constexpr uint8_t D53 = 19; 
+};
+
+#endif /* ARDUINO_H_ */
