0fe66a2184b99ca2497a73ec4b31a9e440be755f
Michał Gumiela <michal.gumiela@gmail.com>
2016-07-16 22:52:36 +0200

    Style corrections

--- HAL/mcu/atmega328p_tqfp32/atmega328p_tqfp32.h
+++ HAL/mcu/atmega328p_tqfp32/atmega328p_tqfp32.h
@@ -7,18 +7,18 @@
 #define PIN_DEF(port, pin) {(int)&DDR ## port, (int)&PORT ## port, (int)&PIN ## port, pin}
 
 constexpr static DigitalIOPinDescriptor DigitalIOPinMap[] = {
-{0, 0, 0, 0},                              //  0 -- artificial pin
+{0, 0, 0, 0},  //  0 -- artificial pin
 PIN_DEF(D,3),  //  1 -- (PCINT19/OC2B/INT1) PD3
 PIN_DEF(D,4),  //  2 -- (PCINT20/XCK/T0) PD4
-{0, 0, 0, 0},                              //  3 -- GND
-{0, 0, 0, 0},                              //  4 -- VCC
-{0, 0, 0, 0},                              //  5 -- GND
-{0, 0, 0, 0},                              //  6 -- VCC
+{0, 0, 0, 0},  //  3 -- GND
+{0, 0, 0, 0},  //  4 -- VCC
+{0, 0, 0, 0},  //  5 -- GND
+{0, 0, 0, 0},  //  6 -- VCC
 PIN_DEF(B,6),  //  7 -- (PCINT6/XTAL1/TOSC1) PB6
 PIN_DEF(B,7),  //  8 -- (PCINT7/XTAL2/TOSC2) PB7
 PIN_DEF(D,5),  //  9 -- (PCINT21/OC0B/T1) PD5
 PIN_DEF(D,6),  // 10 -- (PCINT22/OC0A/AIN0) PD6
-PIN_DEF(D,7),   // 11 -- (PCINT23/AIN1) PD7
+PIN_DEF(D,7),  // 11 -- (PCINT23/AIN1) PD7
 PIN_DEF(B,0),  // 12 -- (PCINT0/CLKO/ICP1) PB0
 PIN_DEF(B,1),  // 13 -- (PCINT1/OC1A) PB1
 PIN_DEF(B,2),  // 14 -- (PCINT2/SS/OC1B) PB2
@@ -39,7 +39,7 @@ PIN_DEF(C,5),  // 28 -- PC5 (ADC5/SCL/PCINT13)
 PIN_DEF(C,6),  // 29 -- PC6 (RESET/PCINT14)
 PIN_DEF(D,0),  // 30 -- PD0 (RXD/PCINT16)
 PIN_DEF(D,1),  // 31 -- PD1 (TXD/PCINT17)
-PIN_DEF(D,2)  // 32 -- PD2 (INT0/PCINT18)
+PIN_DEF(D,2)   // 32 -- PD2 (INT0/PCINT18)
 };
 
 #undef PIN_DEF
