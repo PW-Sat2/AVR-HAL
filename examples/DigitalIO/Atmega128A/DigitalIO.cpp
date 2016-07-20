61684e9345f76c99bbc2ed7787ac5087d285d030
Michał Gumiela <michal.gumiela@gmail.com>
2016-07-20 19:56:28 +0200

    DigitalIO example for Atmega128A added

---
+++ examples/DigitalIO/Atmega128A/DigitalIO.cpp
@@ -0,0 +1,28 @@
+#include <util/delay.h>
+#include "periph/DigitalIO.h"
+
+constexpr hal::DigitalIO pin2(2);
+
+int main() {
+    pin2.init(hal::DigitalIO::OUTPUT);
+    
+    constexpr auto pin3 = hal::DigitalIO(3);
+    pin3.init(hal::DigitalIO::INPUT);
+    
+    // constexpr hal::DigitalIO pinBad(0);  // <- compile error
+	
+	// int i; constexpr hal::DigitalIO pinBad2(i);  // <- compile error
+    
+    while(1) {      
+        pin2.set();
+        pin2.reset();
+        pin2.set();
+        pin2.reset();
+        pin2.set();
+        pin2.reset();
+
+        _delay_ms(200);
+        bool pin_state = pin3.read();
+        _delay_ms(200);
+	}
+}
