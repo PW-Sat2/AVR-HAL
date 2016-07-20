9942962c9b624a2b1ebe4a3f6576bb7f9d8204b3
Michał Gumiela <michal.gumiela@gmail.com>
2016-07-20 19:58:01 +0200

    DigitalIO example filled up similarly to the one for Arduino Mega

--- examples/DigitalIO/ArduinoNano/DigitalIO.cpp
+++ examples/DigitalIO/ArduinoNano/DigitalIO.cpp
@@ -1,24 +1,20 @@
 #include <util/delay.h>
 #include "periph/DigitalIO.h"
-#include "devices/LED.h"
 #include "boards/boards.h"
 
-constexpr hal::DigitalIO pin3(hal::bsp::pins::D3);
+constexpr hal::DigitalIO pinD3(hal::bsp::pins::D3);
 
 int main() {
-    hal::bsp::led.init();
-
-    pin3.init(hal::DigitalIO::OUTPUT);
+    pinD3.init(hal::DigitalIO::INPUT);
     
-    constexpr auto pin2 = hal::DigitalIO(hal::bsp::pins::D2);
+    constexpr auto pin2 = hal::DigitalIO(2);
     pin2.init(hal::DigitalIO::OUTPUT);
     
     // constexpr hal::DigitalIO pinBad(0);  // <- compile error
-	
-	// int i; constexpr hal::DigitalIO pinBad2(i);  // <- compile error
+
+    // int i; constexpr hal::DigitalIO pinBad2(i);  // <- compile error
     
     while(1) {
-        hal::bsp::led.toggle();
         _delay_ms(200);
       
         pin2.set();
@@ -28,11 +24,6 @@ int main() {
         pin2.set();
         pin2.reset();
         
-        pin3.set();
-        pin3.reset();
-        pin3.set();
-        pin3.reset();
-        pin3.set();
-        pin3.reset();
-	}
+        bool pin_state = pinD3.read();
+    }
 }
