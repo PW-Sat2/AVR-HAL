bbf1cd6c8a7353467aab9dc4150475cbc0ef8618
Grzegorz Gajoch <gajoch@gmail.com>
2016-08-09 00:52:50 +0200

    removed old files

--- Examples/Serial0Echo.cpp
+++
@@ -1,8 +0,0 @@
-#include "Serial.h"
-
-int main() {
-	Serial0.begin(9600);
-	while(1) {
-		Serial0.print_byte(Serial0.read_byte());
-	}
-}
--- HAL/AVR/bit_operations.h
+++
@@ -1,9 +0,0 @@
-#ifndef HAL_AVR_BIT_OPERATIONS_H_
-#define HAL_AVR_BIT_OPERATIONS_H_
-
-
-#define sbi(reg, bit) ((reg) |= (1 << (bit)))
-#define cbi(reg, bit) ((reg) &= (~(1 << (bit))))
-#define read_bit(reg, bit)  (((reg) >> bit) & 0x1)
-
-#endif  // HAL_AVR_BIT_OPERATIONS_H_
