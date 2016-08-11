8a8233d2b298413cf612ce7189e34bd8a8cbd139
Grzegorz Gajoch <gajoch@gmail.com>
2016-08-11 21:54:19 +0200

    removed obsolete bit access defines

--- HAL/libs/bit_operations.h
+++ HAL/libs/bit_operations.h
@@ -3,12 +3,6 @@
 
 namespace hal {
 
-
-#define SBI(reg, bit) ((reg) |= (1 << (bit)))
-#define CBI(reg, bit) ((reg) &= (~(1 << (bit))))
-#define RBI(reg, bit)  (((reg) >> bit) & 0x1)
-
-
 template<typename reg_type>
 constexpr void set_bit(reg_type & val, const int bit_pos) {
     val |= (1 << bit_pos);
