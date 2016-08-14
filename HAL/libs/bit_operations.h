17f4665000bac46000667a081956d0c24ee4e263
Grzegorz Gajoch <gajoch@gmail.com>
2016-08-14 19:13:28 +0200

    bit_operations tests

--- HAL/libs/bit_operations.h
+++ HAL/libs/bit_operations.h
@@ -19,6 +19,11 @@ constexpr void clear_bit(reg_type & val, const int bit_pos) {
 }
 
 template<typename reg_type>
+constexpr void clear_bit(reg_type * val, const int bit_pos) {
+    (*val) &= (~(1 << bit_pos));
+}
+
+template<typename reg_type>
 constexpr bool read_bit(const reg_type val, const int bit_pos) {
     return static_cast<bool>((val >> bit_pos) & 0b1);
 }
--- unit_tests/bit_operations.cpp
+++ unit_tests/bit_operations.cpp
@@ -1,6 +1,219 @@
 #include "gtest.h"
-
+#include <cmath>
 #include "bit_operations.h"
+#include <numeric>
+
+TEST(bit_operations, set_bit) {
+    using hal::set_bit;
+    unsigned int val = 0;
+    for (int i = 0; i < 32; ++i) {
+        val = 0;
+        set_bit(val, i);
+        EXPECT_EQ(val, std::pow(2, i));
+    }
+    val = 0;
+    for (int i = 0; i < 32; ++i) {
+        set_bit(val, i);
+    }
+    EXPECT_EQ(val, 0xFFFFFFFF);
+
+    val = 0;
+    for (int i = 0; i < 32; i += 2) {
+        set_bit(val, i);
+    }
+    EXPECT_EQ(val, 0x55555555);
+
+    val = 0;
+    for (int i = 1; i < 32; i += 2) {
+        set_bit(val, i);
+    }
+    EXPECT_EQ(val, 0xAAAAAAAA);
+
+    val = 0;
+    set_bit(val, 0);
+    set_bit(val, 3);
+    set_bit(val, 5);
+    set_bit(val, 7);
+    EXPECT_EQ(val, 0xA9);
+
+    set_bit(val, 0+8);
+    set_bit(val, 3+8);
+    set_bit(val, 5+8);
+    set_bit(val, 7+8);
+    EXPECT_EQ(val, 0xA9A9);
+
+    set_bit(val, 0+16);
+    set_bit(val, 3+16);
+    set_bit(val, 5+16);
+    set_bit(val, 7+16);
+    EXPECT_EQ(val, 0xA9A9A9);
+
+    set_bit(val, 0+24);
+    set_bit(val, 3+24);
+    set_bit(val, 5+24);
+    set_bit(val, 7+24);
+    EXPECT_EQ(val, 0xA9A9A9A9);
+}
+
+TEST(bit_operations, set_bit_ptr) {
+    using hal::set_bit;
+    unsigned int value = 0;
+    unsigned int * val = &value;
+    for (int i = 0; i < 32; ++i) {
+        *val = 0;
+        set_bit(val, i);
+        EXPECT_EQ(*val, std::pow(2, i));
+    }
+    *val = 0;
+    for (int i = 0; i < 32; ++i) {
+        set_bit(val, i);
+    }
+    EXPECT_EQ(*val, 0xFFFFFFFF);
+
+    *val = 0;
+    for (int i = 0; i < 32; i += 2) {
+        set_bit(val, i);
+    }
+    EXPECT_EQ(*val, 0x55555555);
+
+    *val = 0;
+    for (int i = 1; i < 32; i += 2) {
+        set_bit(val, i);
+    }
+    EXPECT_EQ(*val, 0xAAAAAAAA);
+
+    *val = 0;
+    set_bit(val, 0);
+    set_bit(val, 3);
+    set_bit(val, 5);
+    set_bit(val, 7);
+    EXPECT_EQ(*val, 0xA9);
+
+    set_bit(val, 0+8);
+    set_bit(val, 3+8);
+    set_bit(val, 5+8);
+    set_bit(val, 7+8);
+    EXPECT_EQ(*val, 0xA9A9);
+
+    set_bit(val, 0+16);
+    set_bit(val, 3+16);
+    set_bit(val, 5+16);
+    set_bit(val, 7+16);
+    EXPECT_EQ(*val, 0xA9A9A9);
+
+    set_bit(val, 0+24);
+    set_bit(val, 3+24);
+    set_bit(val, 5+24);
+    set_bit(val, 7+24);
+    EXPECT_EQ(*val, 0xA9A9A9A9);
+}
+
+TEST(bit_operations, clear_bit) {
+    using hal::clear_bit;
+    unsigned int val = 0xFFFFFFFF;
+    const auto MAX = std::numeric_limits<decltype(val)>::max();
+    for (int i = 0; i < 32; ++i) {
+        val = 0xFFFFFFFF;
+        clear_bit(val, i);
+        EXPECT_EQ(val, 0xFFFFFFFF-std::pow(2, i));
+    }
+    val = MAX;
+    for (int i = 0; i < 32; ++i) {
+        clear_bit(val, i);
+    }
+    EXPECT_EQ(val, 0);
+
+    val = MAX;
+    for (int i = 0; i < 32; i += 2) {
+        clear_bit(val, i);
+    }
+    EXPECT_EQ(val, 0xAAAAAAAA);
+
+    val = MAX;
+    for (int i = 1; i < 32; i += 2) {
+        clear_bit(val, i);
+    }
+    EXPECT_EQ(val, 0x55555555);
+
+    val = MAX;
+    clear_bit(val, 1);
+    clear_bit(val, 2);
+    clear_bit(val, 4);
+    clear_bit(val, 6);
+    EXPECT_EQ(val, 0xFFFFFFA9);
+
+    clear_bit(val, 1+8);
+    clear_bit(val, 2+8);
+    clear_bit(val, 4+8);
+    clear_bit(val, 6+8);
+    EXPECT_EQ(val, 0xFFFFA9A9);
+
+    clear_bit(val, 1+16);
+    clear_bit(val, 2+16);
+    clear_bit(val, 4+16);
+    clear_bit(val, 6+16);
+    EXPECT_EQ(val, 0xFFA9A9A9);
+
+    clear_bit(val, 1+24);
+    clear_bit(val, 2+24);
+    clear_bit(val, 4+24);
+    clear_bit(val, 6+24);
+    EXPECT_EQ(val, 0xA9A9A9A9);
+}
+
+TEST(bit_operations, clear_bit_ptr) {
+    using hal::clear_bit;
+    unsigned int value = 0xFFFFFFFF;
+    auto val = &value;
+    const auto MAX = std::numeric_limits<decltype(value)>::max();
+    for (int i = 0; i < 32; ++i) {
+        *val = 0xFFFFFFFF;
+        clear_bit(val, i);
+        EXPECT_EQ(*val, 0xFFFFFFFF-std::pow(2, i));
+    }
+    *val = MAX;
+    for (int i = 0; i < 32; ++i) {
+        clear_bit(val, i);
+    }
+    EXPECT_EQ(*val, 0);
+
+    *val = MAX;
+    for (int i = 0; i < 32; i += 2) {
+        clear_bit(val, i);
+    }
+    EXPECT_EQ(*val, 0xAAAAAAAA);
+
+    *val = MAX;
+    for (int i = 1; i < 32; i += 2) {
+        clear_bit(val, i);
+    }
+    EXPECT_EQ(*val, 0x55555555);
+
+    *val = MAX;
+    clear_bit(val, 1);
+    clear_bit(val, 2);
+    clear_bit(val, 4);
+    clear_bit(val, 6);
+    EXPECT_EQ(*val, 0xFFFFFFA9);
+
+    clear_bit(val, 1+8);
+    clear_bit(val, 2+8);
+    clear_bit(val, 4+8);
+    clear_bit(val, 6+8);
+    EXPECT_EQ(*val, 0xFFFFA9A9);
+
+    clear_bit(val, 1+16);
+    clear_bit(val, 2+16);
+    clear_bit(val, 4+16);
+    clear_bit(val, 6+16);
+    EXPECT_EQ(*val, 0xFFA9A9A9);
+
+    clear_bit(val, 1+24);
+    clear_bit(val, 2+24);
+    clear_bit(val, 4+24);
+    clear_bit(val, 6+24);
+    EXPECT_EQ(*val, 0xA9A9A9A9);
+}
 
 TEST(bit_operations, read_bit) {
     int val = 0;
@@ -37,3 +250,4 @@ TEST(bit_operations, read_bit) {
     EXPECT_EQ(hal::read_bit(val2, 6), 0);
     EXPECT_EQ(hal::read_bit(val2, 7), 1);
 }
+
