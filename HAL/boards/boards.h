e692a46e5a634d2511a8e5d758c81d91285eae1d
Ezlur <smialekadam@gmail.com>
2016-09-02 15:40:08 +0200

    add ATxxx4342P support

--- HAL/boards/boards.h
+++ HAL/boards/boards.h
@@ -9,6 +9,8 @@
 #include "easyAVR128/easyAVR128.h"
 #elif defined(BOARD_EVB_MEGA644P_16MHZ)
 #include "EVB/EVB.h"
+#elif defined(ATXXX4324P)
+#include "ATxxx4324P/ATxxx4324P.h"
 #else
 #error "No board defined!"
 #endif
