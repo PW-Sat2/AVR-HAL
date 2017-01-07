4619e403d16ac2a7a31bb73ae85481b4588ba655
Michał Gumiela <michal.gumiela@gmail.com>
2017-01-07 17:18:53 +0100

    SunS-EM3 bsp

--- HAL/boards/SunS_EM3/SunS_EM3.h
+++ HAL/boards/SunS_EM3/SunS_EM3.h
@@ -2,20 +2,23 @@
 #define HAL_BOARDS_SUNS_EM3_SUNS_EM3_H_
 
 #include "periph/DigitalIO.h"
-#include "devices/LED.h"
+#include "periph/Serial.h"
+
+#ifdef DEBUG
+#define printf printf
+#else
+#define printf(text) ((void)0)
+#endif
 
 namespace hal {
-
 namespace bsp {
-
-namespace pins {
 static constexpr InternalADC::Input TEMP_BOARD = InternalADC::Input::ADC4;
 static constexpr InternalADC::Input RTD_AIN_A = InternalADC::Input::ADC3;
 static constexpr InternalADC::Input RTD_AIN_B = InternalADC::Input::ADC2;
 static constexpr InternalADC::Input RTD_AIN_C = InternalADC::Input::ADC1;
 static constexpr InternalADC::Input RTD_AIN_D = InternalADC::Input::ADC0;
 
-
+namespace pins {
 static constexpr DigitalIO::Pin SDA_A1 = 21;
 static constexpr DigitalIO::Pin SDA_B1 = 22;
 static constexpr DigitalIO::Pin SDA_C1 = 23;
@@ -34,9 +37,7 @@ static constexpr DigitalIO::Pin SDA_C3 = 42;
 static constexpr DigitalIO::Pin SDA_D3 = 44;
 static constexpr DigitalIO::Pin SCL_3 = 43;
 
-
 }  // namespace pins
-
 }  // namespace bsp
 }  // namespace hal
 #endif  // HAL_BOARDS_SUNS_EM3_SUNS_EM3_H_
