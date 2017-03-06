b3419f6ec41341e9a43e7992cbeb775da77f792b
Grzegorz Gajoch <gajoch@gmail.com>
2017-03-06 21:45:27 +0000

    Fixed bug in bit_operations

--- HAL/libs/compile_time.h
+++ HAL/libs/compile_time.h
@@ -1,16 +1,18 @@
 #ifndef HAL_LIBS_COMPILE_TIME_H_
 #define HAL_LIBS_COMPILE_TIME_H_
 
+#include <cstdint>
+
 namespace hal {
 namespace libs {
 
-template <int32_t base, int exp>
+template <std::int32_t base, int exp>
 struct power {
     static_assert(base != 0, "Base cannot be equal to 0!");
     static const int32_t value = base * power<base, exp - 1>::value;
 };
 
-template <int32_t base>
+template <std::int32_t base>
 struct power<base, 0> {
     static const int32_t value = 1;
 };
