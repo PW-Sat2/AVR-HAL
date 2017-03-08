074be53e81583d9d5906b1c5bb26c5cb929f58df
Grzegorz Gajoch <gajoch@gmail.com>
2017-03-08 00:47:22 +0000

    Terminal from OBC with slight changes

---
+++ HAL/libs/terminal/terminal.h
@@ -0,0 +1,68 @@
+#ifndef SRC_TERMINAL_H_
+#define SRC_TERMINAL_H_
+
+#include <cstdint>
+#include "span.h"
+
+/**
+ * @defgroup terminal Terminal module
+ *
+ * @{
+ */
+
+/**
+ * @brief Type of function used as terminal command handler
+ */
+using TerminalCommandHandler = void (*)(std::uint8_t argc, char* argv[]);
+
+/**
+ * Structure describing single terminal command
+ */
+struct TerminalCommandDescription
+{
+    /** @brief Name of the command */
+    const char* name;
+    /** Pointer to handler function */
+    TerminalCommandHandler handler;
+};
+
+/**
+ * @brief Terminal class
+ */
+class Terminal
+{
+  public:
+    /**
+     * @brief Sets supported commands list
+     * @param[in] commands Commands list
+     */
+    void SetCommandList(hal::libs::span<const TerminalCommandDescription> commands);
+
+    /**
+     * @brief Starts terminal
+     */
+    void Initialize();
+
+    /**
+     * @brief Handles incoming command
+     * @param[in] buffer Buffer with incoming command
+     *
+     * After this method ends, @p buffer is no longer usable
+     */
+    void HandleCommand(char* buffer);
+
+  private:
+    /**
+     * @brief Sends prefix
+     */
+    void SendPrefix();
+
+    /**
+     * @brief Command list
+     */
+    hal::libs::span<const TerminalCommandDescription> _commandList;
+};
+
+/** @} */
+
+#endif
