8390baf1d75669f709c9b225d9c70a978df76d91
Grzegorz Gajoch <gajoch@gmail.com>
2017-03-11 01:07:06 +0000

    test proving compile-time calculations.

--- unit_tests/compile_time.cpp
+++ unit_tests/compile_time.cpp
@@ -28,23 +28,26 @@ TEST(compile_time, pow) {
 }
 
 
-template<int exp, int size>
-void powerOfTwoTest(uint64_t value) {
+template<int exp, int size, uint64_t value>
+void powerOfTwoTest() {
     constexpr auto res = powerOfTwo<exp>();
-    EXPECT_EQ(sizeof(res), size);
-    EXPECT_EQ(res, value);
-    EXPECT_EQ(powerOfTwo(exp), value);
+    static_assert(res == value, "powerOfTwo (template) failed!");
+    static_assert(sizeof(res) == size, "powerOfTwo (template) incorrect size!");
+
+    constexpr auto res2 = powerOfTwo(exp);
+    static_assert(res2 == value, "powerOfTwo (constexpr) failed!");
+    static_assert(sizeof(res2) == 8, "powerOfTwo (constexpr) incorrect size!");
 }
 
 TEST(compile_time, powerOfTwoTest) {
-    powerOfTwoTest<0, 1>(1);
-    powerOfTwoTest<7, 1>(128);
-    powerOfTwoTest<8, 2>(256);
-    powerOfTwoTest<15, 2>(32768);
-    powerOfTwoTest<16, 4>(65536);
-    powerOfTwoTest<31, 4>(2147483648L);
-    powerOfTwoTest<32, 8>(4294967296ULL);
-    powerOfTwoTest<63, 8>(9223372036854775808ULL);
+    powerOfTwoTest<0, 1, 1>();
+    powerOfTwoTest<7, 1, 128>();
+    powerOfTwoTest<8, 2, 256>();
+    powerOfTwoTest<15, 2, 32768>();
+    powerOfTwoTest<16, 4, 65536>();
+    powerOfTwoTest<31, 4, 2147483648L>();
+    powerOfTwoTest<32, 8, 4294967296ULL>();
+    powerOfTwoTest<63, 8, 9223372036854775808ULL>();
 }
 
 TEST(compile_time, type_with_bits) {
