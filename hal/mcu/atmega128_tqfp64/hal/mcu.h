6584a6402bf74d71b1fa349b0d3a33a3a2dbf323
Michał Gumiela <michal.gumiela@gmail.com>
2017-05-16 01:19:03 +0200

    header guards changed to meet brand new name of the folder

--- hal/mcu/atmega128_tqfp64/hal/mcu.h
+++ hal/mcu/atmega128_tqfp64/hal/mcu.h
@@ -1,5 +1,5 @@
-#ifndef HAL_MCU_ATMEGA128A_TQFP64_HAL_MCU_H_
-#define HAL_MCU_ATMEGA128A_TQFP64_HAL_MCU_H_
+#ifndef HAL_MCU_ATMEGA128_TQFP64_HAL_MCU_H_
+#define HAL_MCU_ATMEGA128_TQFP64_HAL_MCU_H_
 
 #include "mcu_analog.h"
 #include "mcu_core.h"
@@ -9,4 +9,4 @@
 #include "mcu_timers.h"
 #include "mcu_spi.h"
 
-#endif  // HAL_MCU_ATMEGA128A_TQFP64_HAL_MCU_H_
+#endif  // HAL_MCU_ATMEGA128_TQFP64_HAL_MCU_H_
