013c15e7cd2c24c03e97ca316442ec049f1b7114
Grzegorz Gajoch <gajoch@gmail.com>
2017-05-30 00:31:04 +0100

    FPF270x fix, unit tests added

--- hal/devices.h
+++ hal/devices.h
@@ -2,13 +2,14 @@
 #define HAL_DEVICES_H_
 
 #include "hal/devices/AD5641.h"
-#include "hal/devices/AD7714-ext.h"
 #include "hal/devices/AD7714.h"
+#include "hal/devices/AD7714-ext.h"
 #include "hal/devices/ADG708.h"
 #include "hal/devices/ADG709.h"
 #include "hal/devices/ADXRS453.h"
 #include "hal/devices/AT24C02.h"
 #include "hal/devices/DAC1220.h"
+#include "hal/devices/FPF270x.h"
 #include "hal/devices/LED.h"
 #include "hal/devices/PCF8563.h"
 
