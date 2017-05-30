6c14ecbfa5bf7dcfce6296ff702a000ccc7d6195
Grzegorz Gajoch <gajoch@gmail.com>
2017-05-30 01:38:42 +0100

    SingleDevice

--- tests/SingleDevice/main.cpp
+++ tests/SingleDevice/main.cpp
@@ -1,14 +1,19 @@
 #include <hal/hal>
 #include "tests.h"
 
-__extension__ typedef int __guard __attribute__((mode (__DI__)));
+__extension__ typedef int __guard __attribute__((mode(__DI__)));
 
-extern "C" int __cxa_guard_acquire(__guard *);
-extern "C" void __cxa_guard_release(__guard *);
-extern "C" void __cxa_guard_abort(__guard *);
-int __cxa_guard_acquire(__guard *g) {return !*reinterpret_cast<char *>(g);}
-void __cxa_guard_release(__guard *g) {*reinterpret_cast<char *>(g) = 1;}
-void __cxa_guard_abort(__guard *) {}
+extern "C" int __cxa_guard_acquire(__guard*);
+extern "C" void __cxa_guard_release(__guard*);
+extern "C" void __cxa_guard_abort(__guard*);
+int __cxa_guard_acquire(__guard* g) {
+    return !*reinterpret_cast<char*>(g);
+}
+void __cxa_guard_release(__guard* g) {
+    *reinterpret_cast<char*>(g) = 1;
+}
+void __cxa_guard_abort(__guard*) {
+}
 
 typedef void (*fptr)();
 
--- tests/SingleDevice/tests/DigitalIO.cpp
+++ tests/SingleDevice/tests/DigitalIO.cpp
@@ -38,12 +38,12 @@ TEST(DigitalIO, mosi_as_output) {
     miso.init(Interface::Mode::INPUT);
     mosi.init(Interface::Mode::OUTPUT);
 
-    mosi.set();    TEST_ASSERT_EQUAL_UINT8(true,  miso.read());
-    mosi.reset();  TEST_ASSERT_EQUAL_UINT8(false, miso.read());
-    mosi.write(0); TEST_ASSERT_EQUAL_UINT8(false, miso.read());
-    mosi.write(1); TEST_ASSERT_EQUAL_UINT8(true,  miso.read());
-    mosi.toggle(); TEST_ASSERT_EQUAL_UINT8(false, miso.read());
-    mosi.toggle(); TEST_ASSERT_EQUAL_UINT8(true,  miso.read());
+    mosi.set(), TEST_ASSERT_EQUAL_UINT8(true, miso.read());
+    mosi.reset(), TEST_ASSERT_EQUAL_UINT8(false, miso.read());
+    mosi.write(0), TEST_ASSERT_EQUAL_UINT8(false, miso.read());
+    mosi.write(1), TEST_ASSERT_EQUAL_UINT8(true, miso.read());
+    mosi.toggle(), TEST_ASSERT_EQUAL_UINT8(false, miso.read());
+    mosi.toggle(), TEST_ASSERT_EQUAL_UINT8(true, miso.read());
 
     reset();
 }
@@ -52,13 +52,12 @@ TEST(DigitalIO, miso_as_output) {
     mosi.init(Interface::Mode::INPUT);
     miso.init(Interface::Mode::OUTPUT);
 
-    miso.set();    TEST_ASSERT_EQUAL_UINT8(true,  mosi.read());
-    miso.reset();  TEST_ASSERT_EQUAL_UINT8(false, mosi.read());
-    miso.write(0); TEST_ASSERT_EQUAL_UINT8(false, mosi.read());
-    miso.write(1); TEST_ASSERT_EQUAL_UINT8(true,  mosi.read());
-    miso.toggle(); TEST_ASSERT_EQUAL_UINT8(false, mosi.read());
-    miso.toggle(); TEST_ASSERT_EQUAL_UINT8(true,  mosi.read());
+    miso.set(), TEST_ASSERT_EQUAL_UINT8(true, mosi.read());
+    miso.reset(), TEST_ASSERT_EQUAL_UINT8(false, mosi.read());
+    miso.write(0), TEST_ASSERT_EQUAL_UINT8(false, mosi.read());
+    miso.write(1), TEST_ASSERT_EQUAL_UINT8(true, mosi.read());
+    miso.toggle(), TEST_ASSERT_EQUAL_UINT8(false, mosi.read());
+    miso.toggle(), TEST_ASSERT_EQUAL_UINT8(true, mosi.read());
 
     reset();
 }
-
