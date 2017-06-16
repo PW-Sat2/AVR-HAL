7a61efb60fc8b9d32a1ac4c4980f07dd15f9eea3
Piotr Kuligowski <kontakt@piotrkuligowski.pl>
2017-06-16 05:52:04 +0200

    num() used, uint16_t replaced to uint12_t, static SPI solution used, NormalOperation mode removed, namespaces fixed

--- hal/devices/DAC121.h
+++ hal/devices/DAC121.h
@@ -5,45 +5,36 @@
 #include "hal/periph.h"
 
 namespace hal {
-namespace drivers {
+namespace devices {
+namespace DAC121 {
 
-class DAC121 {
- public:
-    explicit DAC121(SPI::Interface& spi_dev) : spi_dev{spi_dev} {
+enum class PowerDownMode : std::uint8_t {
+    ToGnd1k   = 1 << 4,
+    ToGnd100k = 2 << 4,
+    HiZ       = 3 << 4
+};
+
+template<typename SPI>
+struct DAC121 : libs::PureStatic {
+    static void set_power_down_mode(PowerDownMode new_power_down_mode) {
+        const std::array<std::uint8_t, 2> data_out = {num(new_power_down_mode), 0};
+
+        SPI::write(data_out);
     }
 
-    enum class PowerDownMode : std::uint8_t {
-        ToGnd1k   = 1 << 4,
-        ToGnd100k = 2 << 4,
-        HiZ       = 3 << 4
-    };
-
-    void set_power_down_mode(PowerDownMode new_power_down_mode) {
-        const std::array<std::uint8_t, 2> data_out = {
-            static_cast<std::uint8_t>(static_cast<std::uint8_t>(new_power_down_mode)),
-            0};
-
-        spi_dev.write(data_out);
-    }
-
-    void write_to_output(std::uint16_t raw_value) {
+    static void write_to_output(uint12_t raw_value) {
         std::array<std::uint8_t, 2> data_out;
         libs::Writer writer{data_out};
 
-        writer.WriteByte(static_cast<std::uint8_t>(OperationMode::NormalOperation) |
-                         ((raw_value >> 8) & 0x0F));
+        writer.WriteByte((raw_value >> 8) & 0x0F);
         writer.WriteByte(raw_value & 0xFF);
 
-        spi_dev.write(data_out);
+        SPI::write(data_out);
     }
-
- private:
-    SPI::Interface& spi_dev;
-
-    enum class OperationMode : std::uint8_t { NormalOperation = 0 << 4 };
 };
 
-}  // namespace drivers
+};  // namespace DAC121
+}  // namespace devices
 }  // namespace hal
 
 #endif  // HAL_DEVICES_DAC121_H_
--- unit_tests/drivers/DAC121.cpp
+++ unit_tests/drivers/DAC121.cpp
@@ -6,10 +6,10 @@ TEST_GROUP(DAC121);
 
 using namespace hal;
 using namespace hal::libs;
-using namespace hal::drivers;
+using namespace hal::devices;
 
 struct DAC121Mock : public EmptySPIMock {
-    void write(gsl::span<const uint8_t> in) override {
+    static void write(gsl::span<const uint8_t> in) {
         uint16_t word;
         TEST_ASSERT_EQUAL_INT(2, in.size());
 
@@ -20,79 +20,81 @@ struct DAC121Mock : public EmptySPIMock {
         value = word & 0xFFF;
     }
 
-    uint16_t value;
-    uint8_t mode;
+    static uint16_t value;
+    static uint8_t mode;
 };
+uint16_t DAC121Mock::value;
+uint8_t DAC121Mock::mode;
 
 TEST(DAC121, modes) {
-    DAC121Mock mock;
-    DAC121 dac121(mock);
+    using mock   = DAC121Mock;
+    using dac121 = DAC121::DAC121<mock>;
 
     // 2 cases for Normal Operation
-    dac121.write_to_output(111);
-    TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+    dac121::write_to_output(111);
+    TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
 
-    dac121.write_to_output(0);
-    TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+    dac121::write_to_output(0);
+    TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
 
     // Power Down mode with 1k to GND
-    dac121.set_power_down_mode(DAC121::PowerDownMode::ToGnd1k);
-    TEST_ASSERT_EQUAL_INT(0x1, mock.mode);
+    dac121::set_power_down_mode(DAC121::PowerDownMode::ToGnd1k);
+    TEST_ASSERT_EQUAL_INT(0x1, mock::mode);
 
     // Power Down mode with 100k to GND
-    dac121.set_power_down_mode(DAC121::PowerDownMode::ToGnd100k);
-    TEST_ASSERT_EQUAL_INT(0x2, mock.mode);
+    dac121::set_power_down_mode(DAC121::PowerDownMode::ToGnd100k);
+    TEST_ASSERT_EQUAL_INT(0x2, mock::mode);
 
     // Power Down mode with HiZ
-    dac121.set_power_down_mode(DAC121::PowerDownMode::HiZ);
-    TEST_ASSERT_EQUAL_INT(0x3, mock.mode);
+    dac121::set_power_down_mode(DAC121::PowerDownMode::HiZ);
+    TEST_ASSERT_EQUAL_INT(0x3, mock::mode);
 }
 
 TEST(DAC121, edgeCases) {
-    DAC121Mock mock;
-    DAC121 dac121(mock);
+    using mock   = DAC121Mock;
+    using dac121 = DAC121::DAC121<mock>;
 
-    dac121.write_to_output(4095);
-    TEST_ASSERT_EQUAL_INT(4095, mock.value);
-    TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+    dac121::write_to_output(4095);
+    TEST_ASSERT_EQUAL_INT(4095, mock::value);
+    TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
 
-    dac121.write_to_output(0);
-    TEST_ASSERT_EQUAL_INT(0, mock.value);
-    TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+    dac121::write_to_output(0);
+    TEST_ASSERT_EQUAL_INT(0, mock::value);
+    TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
 }
 
 TEST(DAC121, outOfRange) {
-    DAC121Mock mock;
-    DAC121 dac121(mock);
+    using mock   = DAC121Mock;
+    using dac121 = DAC121::DAC121<mock>;
 
-    dac121.write_to_output(4096);
-    TEST_ASSERT_EQUAL_INT(0, mock.value);
-    TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+    dac121::write_to_output(4096);
+    TEST_ASSERT_EQUAL_INT(0, mock::value);
+    TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
 
-    dac121.write_to_output(22403);
-    TEST_ASSERT_EQUAL_INT(1923, mock.value);
-    TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+    dac121::write_to_output(22403);
+    TEST_ASSERT_EQUAL_INT(1923, mock::value);
+    TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
 
-    dac121.write_to_output(65535);
-    TEST_ASSERT_EQUAL_INT(4095, mock.value);
-    TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+    dac121::write_to_output(65535);
+    TEST_ASSERT_EQUAL_INT(4095, mock::value);
+    TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
 
-    dac121.write_to_output(-1);
-    TEST_ASSERT_EQUAL_INT(4095, mock.value);
-    TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+    dac121::write_to_output(-1);
+    TEST_ASSERT_EQUAL_INT(4095, mock::value);
+    TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
 
-    dac121.write_to_output(-3);
-    TEST_ASSERT_EQUAL_INT(4093, mock.value);
-    TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+    dac121::write_to_output(-3);
+    TEST_ASSERT_EQUAL_INT(4093, mock::value);
+    TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
 }
 
 TEST(DAC121, fullRange) {
-    DAC121Mock mock;
-    DAC121 dac121(mock);
+    using mock   = DAC121Mock;
+    using dac121 = DAC121::DAC121<mock>;
 
     for (uint16_t i = 0; i < 4096; ++i) {
-        dac121.write_to_output(i);
-        TEST_ASSERT_EQUAL_INT(i, mock.value);
-        TEST_ASSERT_EQUAL_INT(0x0, mock.mode);
+        dac121::write_to_output(i);
+        TEST_ASSERT_EQUAL_INT(i, mock::value);
+        TEST_ASSERT_EQUAL_INT(0x0, mock::mode);
     }
 }
