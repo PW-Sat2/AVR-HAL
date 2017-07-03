4f44cd48a126f49b6d8df09d37e778613cd77b19
Michał Gumiela <michal.gumiela@gmail.com>
2017-07-03 11:15:20 +0200

    - 139#discussion_r125182939 - 139#discussion_r125182606 - 139#discussion_r125183098

--- hal/devices/AD7714.h
+++ hal/devices/AD7714.h
@@ -57,28 +57,49 @@ enum class Polarity : std::uint8_t {
     UNIPOLAR = 1,
 };
 
-template<typename spi_dev, typename pin_DRDY>
+template<typename SPI, typename pin_DRDY>
 class AD7714 {
  public:
-    void change_channel(Channels channel) {
-        this->actual_channel = channel;
-        write_to_comm_reg(Registers::COMM_REG, true);
-
-        std::uint8_t data;
-        spi_dev::read(data);
-    }
-
-
     void init() const {
         pin_DRDY::init(DigitalIO::Mode::INPUT);
     }
 
 
-    uint24_t read_data_no_wait() {
-        write_to_comm_reg(Registers::DATA_REG, true);
+    void change_channel(Channels channel) {
+        this->actual_channel = channel;
+        std::uint8_t data;
+        this->read_register(Registers::COMM_REG, data);
+    }
 
+
+    template<std::uint16_t filter>
+    void set_filter(const Polarity set_polarity) {
+        static_assert((filter >= 19) && (filter <= 4000),
+                      "Allowed filter value is 19 - 4000");
+
+        std::uint8_t val = (num(set_polarity) << 7) |
+                           (this->word_len_24b << 6) | (filter >> 8);
+        this->write_register(Registers::FILTER_HIGH_REG, val);
+
+        val = libs::read_mask<0, 8>(filter);
+        this->write_register(Registers::FILTER_LOW_REG, val);
+    }
+
+
+    void set_mode(const Modes mode, const Gain gain) {
+        std::uint8_t data = (num(mode) << 5) | (num(gain) << 2);
+        this->write_register(Registers::MODE_REG, data);
+    }
+
+
+    bool data_ready() {
+        return !pin_DRDY::read();
+    }
+
+
+    uint24_t read_data_no_wait() {
         std::array<std::uint8_t, 3> data;
-        spi_dev::read(data);
+        this->read_register(Registers::DATA_REG, data);
 
         std::uint32_t read = 0;
         read |= data[0], read <<= 8;
@@ -87,42 +108,22 @@ class AD7714 {
         return static_cast<uint24_t>(read);
     }
 
-
-    void set_mode(const Modes mode, const Gain gain) {
-        write_to_comm_reg(Registers::MODE_REG, false);
-        std::uint8_t data = (static_cast<std::uint8_t>(mode) << 5) |
-                            (static_cast<std::uint8_t>(gain) << 2);
-        spi_dev::write(data);
-    }
-
-    template<std::uint16_t filter>
-    void set_filter(const Polarity set_polarity) {
-        static_assert((filter >= 19) && (filter <= 4000),
-                      "Allowed filter value is 19 - 4000");
-
-        write_to_comm_reg(Registers::FILTER_HIGH_REG, false);
-        std::uint8_t val = (static_cast<std::uint8_t>(set_polarity) << 7) |
-                           (1 << 6) | (1 << 5) | libs::read_mask<7, 4>(filter);
-        spi_dev::write(val);
-
-        write_to_comm_reg(Registers::FILTER_LOW_REG, false);
-        val = libs::read_mask<0, 8>(filter);
-        spi_dev::write(val);
-    }
-
-    bool data_ready() {
-        return !pin_DRDY::read();
-    }
-
-
  private:
     Channels actual_channel;
+    constexpr static bool word_len_24b = true;
 
-    void write_to_comm_reg(const Registers reg, const bool read) const {
-        std::uint8_t out = (static_cast<std::uint8_t>(reg) << 4) |
-                           (static_cast<std::uint8_t>(read) << 3) |
-                           (static_cast<std::uint8_t>(actual_channel));
-        spi_dev::write(out);
+    template<typename T>
+    void read_register(const Registers reg, T& data) const {
+        std::uint8_t out = (num(reg) << 4) | (1 << 3) | (num(actual_channel));
+        SPI::write(out);
+        SPI::read(data);
+    }
+
+    template<typename T>
+    void write_register(const Registers reg, T& data) const {
+        std::uint8_t out = (num(reg) << 4) | (num(actual_channel));
+        SPI::write(out);
+        SPI::write(data);
     }
 };
 
