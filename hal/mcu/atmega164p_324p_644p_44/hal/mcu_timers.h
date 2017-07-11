9cab032572b8759e5607d25f98a8382fcc207861
Grzegorz Gajoch <gajoch@gmail.com>
2017-07-11 10:51:31 +0100

    Added Timer1 implementation

--- hal/mcu/atmega164p_324p_644p_44/hal/mcu_timers.h
+++ hal/mcu/atmega164p_324p_644p_44/hal/mcu_timers.h
@@ -49,6 +49,63 @@ class Timer0 {
     }
 };
 
+class Timer1 {
+ public:
+    enum class Prescaler : uint8_t {
+        No_Clock   = 0,
+        DIV_1      = 1,
+        DIV_8      = 2,
+        DIV_64     = 3,
+        DIV_256    = 4,
+        DIV_1024   = 5,
+        T1_FALLING = 6,
+        T1_RISING  = 7,
+    };
+
+    enum class Mode : uint8_t {
+        Normal                                    = 0,
+        PWM_phase_correct_8b                      = 1,
+        PWM_phase_correct_9b                      = 2,
+        PWM_phase_correct_10b                     = 3,
+        CTC_OCRnA_TOP                             = 4,
+        FastPWM_8b                                = 5,
+        FastPWM_9b                                = 6,
+        FastPWM_10b                               = 7,
+        PWM_phase_and_frequency_correct_ICRn_TOP  = 8,
+        PWM_phase_and_frequency_correct_OCRnA_TOP = 9,
+        PWM_phase_correct_ICRn_TOP                = 10,
+        PWM_phase_correct_OCRnA_TOP               = 11,
+        CTC_ICRn_TOP                              = 12,
+        FastPWM_ICRn_TOP                          = 14,
+        FastPWM_OCRnA_TOP                         = 15
+    };
+
+    static void init(Prescaler prescaler, Mode mode) {
+        TCCR1A = num(mode) & 0b0011;
+        TCCR1B = ((num(mode) & 0b1100) << 1) | num(prescaler);
+    }
+
+    static void set_output_compare_A(uint16_t value) {
+        OCR1A = value;
+    }
+
+    static void set_output_compare_B(uint16_t value) {
+        OCR1B = value;
+    }
+
+    static void enable_overflow_interrupt() {
+        libs::set_bit(TIMSK1, TOIE1);
+    }
+
+    static void enable_compare_A_interrupt() {
+        libs::set_bit(TIMSK1, OCIE1A);
+    }
+
+    static void enable_compare_B_interrupt() {
+        libs::set_bit(TIMSK1, OCIE1B);
+    }
+};
+
 }  // namespace mcu
 }  // namespace hal
 
